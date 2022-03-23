040f28a3e452544b7b15b1275a2de322d3b186d1
Oldli <wenju1991@163.com>
2022-03-23 22:14:41 +0800

    修复bug

--- plugins/load-locales.js
+++ plugins/load-locales.js
@@ -36,37 +36,42 @@ export default function (ctx) {
             mods.push('doc')
           }
         } else {
+          const { entryModule, dependencyModules, skins } = ctx
+
           /** 导入Mkh中的语言包 */
           code += `import ui from '${UI_NAME}/lib/locales/${lang}'\r\n`
 
           /** 导入当前模块中的语言包 */
           const src = normalizePath(path.resolve(process.cwd(), `src/locales/${lang}/index.js`))
           if (fs.existsSync(src)) {
-            code += `import ${ctx.entryModule} from '${src}'\r\n`
-            mods.push(ctx.entryModule)
+            code += `import ${entryModule} from '${src}'\r\n`
+            mods.push(entryModule)
+          }
+          /** 导入依赖模块中的语言包 */
+          if (dependencyModules) {
+            dependencyModules.forEach(m => {
+              let moduleName = `${MODULE_PREFIX}${m}`
+              const src = normalizePath(path.resolve(process.cwd(), `node_modules/${moduleName}/lib/locales/${lang}/index.js`))
+              if (fs.existsSync(src)) {
+                let moduleImportName = moduleName.replaceAll('-', '_')
+                code += `import ${moduleImportName} from '${src}'\r\n`
+                mods.push(moduleImportName)
+              }
+            })
           }
 
-          /** 导入依赖模块中的语言包 */
-          ctx.dependencyModules.forEach(m => {
-            let moduleName = `${MODULE_PREFIX}${m}`
-            const src = normalizePath(path.resolve(process.cwd(), `node_modules/${moduleName}/lib/locales/${lang}/index.js`))
-            if (fs.existsSync(src)) {
-              let moduleImportName = moduleName.replaceAll('-', '_')
-              code += `import ${moduleImportName} from '${src}'\r\n`
-              mods.push(moduleImportName)
-            }
-          })
-
           /** 导入皮肤中的语言包 */
-          ctx.skins.forEach(skin => {
-            let skinName = `${SKIN_PREFIX}${skin}`
-            const src = normalizePath(path.resolve(process.cwd(), `node_modules/${skinName}/lib/locales/${lang}/index.js`))
-            if (fs.existsSync(src)) {
-              let skinImportName = skinName.replaceAll('-', '_')
-              code += `import ${skinImportName} from '${skinName}/lib/locale/${lang}'\r\n`
-              skinList += `${skin.replace(SKIN_PREFIX, '')}: ${skinImportName}`
-            }
-          })
+          if (skins) {
+            skins.forEach(skin => {
+              let skinName = `${SKIN_PREFIX}${skin}`
+              const src = normalizePath(path.resolve(process.cwd(), `node_modules/${skinName}/lib/locales/${lang}/index.js`))
+              if (fs.existsSync(src)) {
+                let skinImportName = skinName.replaceAll('-', '_')
+                code += `import ${skinImportName} from '${skinName}/lib/locale/${lang}'\r\n`
+                skinList += `${skin.replace(SKIN_PREFIX, '')}: ${skinImportName}`
+              }
+            })
+          }
         }
 
         code += `mkh.localeMessages.push({el:el.el, ui, mod: {${mods.join(',')}}, skin: {${skinList}}})`
